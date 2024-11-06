82a0ffe064ed3551b3ad3059326ca2b8a1919229
vmartinez <victormartinez12321@gmail.com>
2024-11-06 23:13:49 +0100

    Sistema de bombas y puntuación

--- src/Main.java
+++ src/Main.java
@@ -2,24 +2,27 @@ import java.util.Random;
 import java.util.Scanner;
 
 /*
-Haz un programa en Java que solicite por consola al usuario dos números (filas, columnas) enteros que
-representarán las filas y columnas que tendrá el terreno del juego. Si el usuario escoge un valor no válido,
-le indicaremos que este valor es incorrecto y le volveremos a pedir la información.
+    Haz un programa en Java que solicite por consola al usuario dos números (filas, columnas) enteros que
+    representarán las filas y columnas que tendrá el terreno del juego. Si el usuario escoge un valor no válido,
+    le indicaremos que este valor es incorrecto y le volveremos a pedir la información.
 */
 
 public class Main {
     public static void main(String[] args) {
+        // Inicialización de clases
         Scanner input = new Scanner(System.in);
         Random rand  = new Random();
+        // Variables de formato de texto
         final String ANSI_RED = "\u001B[31m";
         final String ANSI_RESET = "\u001B[0m";
         final String ANSI_GREEN = "\u001B[32m";
-
+        // Declaración de variables
         boolean end = false;
         int height;
         int width;
         int coordX;
         int coordY;
+        int score;
 
         // Generar terreno de juego
 
@@ -62,91 +65,95 @@ public class Main {
             System.out.println();
         }
 
-    while (!end) {
-        // Menú de acciones
-        int action;
-        do { // Utilizamos un bucle de
-            System.out.println(""" 
-                            ---------
-                            [2] Poner bomba
-                            [1] Mostrar matriz
-                            [0] Salir
-                            ---------
-                            """);
-            if (input.hasNextInt()) {
-                action = input.nextInt();
-                if (action >= 0 && action <= 2) {
-                    input.nextLine(); // Limpiamos el búfer
+        while (!end) {
+            // Menú de acciones
+            int action;
+            do { // Utilizamos un bucle de
+                System.out.println(""" 
+                                ---------
+                                [2] Poner bomba
+                                [1] Mostrar matriz
+                                [0] Salir
+                                ---------
+                                """);
+                if (input.hasNextInt()) {
+                    action = input.nextInt();
+                    if (action >= 0 && action <= 2) {
+                        input.nextLine(); // Limpiamos el búfer
+                        break;
+                    }
+                }
+                System.out.println(ANSI_RED + "Error. Introduce un número entre 2-0" + ANSI_RESET);
+                input.nextLine();//Limpiamos el búfer
+            } while (true);
+            switch (action) {
+                case 2: // Poner bomba
+                    // Pedimos la coordenada X
+                    do { // Utilizamos un bucle para comprobar el formato del valor introducido por el usuario
+                        System.out.println("Coordenada X: ");
+                        if (input.hasNextInt()) {
+                            coordX = input.nextInt();
+                            //Las coordenadas que introduce el usuario, no corresponden a la misma posición en el array
+                            //hemos de tener en cuenta que la primera posición, será (1,1) para el usuario, lo que corresponde a la posición [0][0] en el array
+                            if (coordX >= 1&&coordX<=width) {
+                                --coordX; // Le restamos 1 al valor, para que corresponda con su posición en el array
+                                input.nextLine(); // Limpiamos el búfer
+                                break;
+                            }
+                        }
+                        System.out.println(ANSI_RED + "Error. Introduce un número entre 1 y "+width +"."+ANSI_RESET);
+                        input.nextLine();//Limpiamos el búfer
+                    } while (true);
+
+                    // Pedimos la coordenada Y
+                    do { // Utilizamos un bucle para comprobar el formato del valor introducido por el usuario
+                        System.out.println("Coordenada Y:");
+                        System.out.print((coordX+1)+",");
+                        if (input.hasNextInt()) {
+                            coordY = input.nextInt();
+                            if (coordY >= 1&&coordY<=height) {
+                                --coordY; // Le restamos 1 al valor, para que corresponda con su posición en el array
+                                input.nextLine(); // Limpiamos el búfer
+                                break;
+                            }
+                        }
+                        System.out.println(ANSI_RED + "Error. Introduce un número entre 1 y "+height +"."+ANSI_RESET);
+                        input.nextLine();//Limpiamos el búfer
+                    } while (true);
+
+                    // Explosión de la bomba
+                    score = 0;
+                    for (int row=0; row < terrain.length;++row) {
+                        for (int col=0; col<terrain[0].length; ++col) {
+                            if(col==coordX||row==coordY){
+                                score += terrain[row][col];
+                                terrain[row][col]= 0;
+                            }
+                        }
+                    }
+                    System.out.println("Valor explosión:"+score);
+
                     break;
-                }
+                case 1: // Mostrar matriz
+                    // Usamos dos bucles foreach para imprimir los valores  del array por pantalla
+                    for (int[] row : terrain) {  // El primer bucle, almacena una fila del array2D en un array int por cada iteración
+                        for (int elem : row) { // El segundo bucle, itera sobre cada número del array de la fila
+                            if (elem==0){ // Imprime cada número por pantalla, separado por espacios, de color rojo si es un 0
+                                System.out.print(ANSI_RED+elem + " ");
+                            }else{
+                                System.out.print(ANSI_GREEN+elem + " ");
+                            }
+                        }
+                        System.out.println(ANSI_RESET); // Realiza un salto de línea para imprimir la siguiente fila en el array, devuelve el color original al tetxo de la consola
+                    }
+                    break;
+                case 0: // Salir del programa
+                    end = true;
+                    break;
+                default:
+                    System.out.println(ANSI_RED + "Error. Acción inválida" + ANSI_RESET);
+                    break;
             }
-            System.out.println(ANSI_RED + "Error. Introduce un número entre 2-0" + ANSI_RESET);
-            input.nextLine();//Limpiamos el búfer
-        } while (true);
-        switch (action) {
-            case 2: // Poner bomba
-                // Pedimos la coordenada X
-                do { // Utilizamos un bucle para comprobar el formato del valor introducido por el usuario
-                    System.out.println("Coordenada X: ");
-                    if (input.hasNextInt()) {
-                        coordX = input.nextInt();
-                        if (coordX > 1&&coordX<width) {
-                            --coordX; // Le restamos uno al valor, para que corresponda con su posición en el array
-                            input.nextLine(); // Limpiamos el búfer
-                            break;
-                        }
-                    }
-                    System.out.println(ANSI_RED + "Error. Introduce un número entre 1 y "+width +"."+ANSI_RESET);
-                    input.nextLine();//Limpiamos el búfer
-                } while (true);
-
-                // Pedimos la coordenada Y
-                do { // Utilizamos un bucle para comprobar el formato del valor introducido por el usuario
-                    System.out.println("Coordenada Y:");
-                    System.out.print(coordX+",");
-                    if (input.hasNextInt()) {
-                        coordY = input.nextInt();
-                        if (coordY > 1&&coordY<height) {
-                            --coordY; // Le restamos uno al valor, para que corresponda con su posición en el array
-                            input.nextLine(); // Limpiamos el búfer
-                            break;
-                        }
-                    }
-                    System.out.println(ANSI_RED + "Error. Introduce un número entre 1 y "+height +"."+ANSI_RESET);
-                    input.nextLine();//Limpiamos el búfer
-                } while (true);
-
-                // Explosión de la bomba
-                for (int row=0; row < terrain.length;++row) {
-                    for (int col=0; col<terrain[0].length; ++col) {
-                        if(row==coordX||col==coordY){
-                            terrain[row][col]= 0;
-                        }
-                    }
-                }
-
-                break;
-            case 1: // Mostrar matriz
-                // Usamos dos bucles foreach para imprimir los valores  del array por pantalla
-                for (int[] row : terrain) {  // El primer bucle, almacena una fila del array2D en un array int por cada iteración
-                    for (int elem : row) { // El segundo bucle, itera sobre cada número del array de la fila
-                        if (elem==0){ // Imprime cada número por pantalla, separado por espacios, de color rojo si es un 0
-                            System.out.print(ANSI_RED+elem + " ");
-                        }else{
-                            System.out.print(ANSI_GREEN+elem + " ");
-                        }
-                    }
-                    System.out.println(ANSI_RESET); // Realiza un salto de línea para imprimir la siguiente fila en el array, devuelve el color original al tetxo de la consola
-                }
-                break;
-            case 0: // Salir del programa
-                end = true;
-                break;
-            default:
-                System.out.println(ANSI_RED + "Error. Acción inválida" + ANSI_RESET);
-                break;
         }
-    }
-
     }
 }
